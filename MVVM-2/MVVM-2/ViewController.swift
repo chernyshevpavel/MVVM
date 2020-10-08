f205b6e574f5749353189c2ecc15cf01688611df
Pavel Chernyshev <titan100695@yandex.ru>
2020-10-08 22:45:25 +0300

    init commit

---
+++ MVVM-2/MVVM-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-2
+//
+//  Created by Павел Чернышев on 08.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
