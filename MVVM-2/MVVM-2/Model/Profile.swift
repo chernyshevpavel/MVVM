76fbd97b59ad5ff3e30cce9bcd10c707c550cd6b
Pavel Chernyshev <titan100695@yandex.ru>
2020-10-08 23:07:15 +0300

    mvc pattern

---
+++ MVVM-2/MVVM-2/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM-2
+//
+//  Created by Павел Чернышев on 08.10.2020.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM-2/MVVM-2/View/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  MVVM-2
+//
+//  Created by Павел Чернышев on 08.10.2020.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+
+}
